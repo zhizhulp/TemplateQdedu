bb0f473f3553fe38319f348ba3a5e30eaad2e007
GitHub Action <action@github.com>
2021-04-25 08:56:44 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhizhulp/templateqdedu/MyBundle.kt
+++ src/main/kotlin/com/github/zhizhulp/templateqdedu/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.zhizhulp.templateqdedu
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/zhizhulp/templateqdedu/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/zhizhulp/templateqdedu/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.zhizhulp.templateqdedu.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.zhizhulp.templateqdedu.MyBundle
 
 class MyApplicationService {
 
